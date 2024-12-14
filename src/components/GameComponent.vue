f18351514990c74f5bc67b633368b75741a0e791
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-12-14 19:03:56 +0200

    feat: create GameComponent with all logic, which previously be in App.vue

---
+++ src/components/GameComponent.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import {
+  checkTie,
+  checkWin,
+  getDefaultBoard,
+  getEmptySmallBoard,
+  getNextBoardIndex,
+  getWinnerBoard,
+  isValidMove,
+} from '@/TicTacToe/GameController'
+import { type Sign } from '@/TicTacToe/types'
+import { reactive, ref } from 'vue'
+import GameField from '@/components/GameField.vue'
+
+const winner = ref<string | null>(null)
+const isTie = ref<boolean>(false)
+const gameOver = ref<boolean>(false)
+const currentPlayer = ref<Sign>('X')
+
+const currentBoard = ref<number | null>(null)
+
+let board = reactive(getDefaultBoard())
+let winBoard = reactive(getEmptySmallBoard())
+
+const playMove = (smallBoardIndex: number, row: number, col: number) => {
+  const validMove = isValidMove(
+    smallBoardIndex,
+    row,
+    col,
+    gameOver.value,
+    board,
+    currentBoard.value,
+  )
+
+  if (validMove) {
+    board[smallBoardIndex][row][col] = currentPlayer.value
+    winBoard = reactive(getWinnerBoard(board))
+
+    if (checkWin(winBoard, currentPlayer.value)) {
+      winner.value = currentPlayer.value
+      gameOver.value = true
+    } else if (checkTie(board)) {
+      isTie.value = true
+      gameOver.value = true
+    } else {
+      currentPlayer.value = currentPlayer.value === 'X' ? 'O' : 'X'
+    }
+
+    currentBoard.value = getNextBoardIndex(board, row, col)
+  }
+}
+
+const reset = () => {
+  board = reactive(getDefaultBoard())
+  winBoard = reactive(getEmptySmallBoard())
+  currentPlayer.value = 'X'
+  gameOver.value = false
+  winner.value = null
+  isTie.value = false
+  currentBoard.value = null
+}
+</script>
+
+<template>
+  <div>
+    <p class="current-player">
+      Current Player: <span class=""> {{ currentPlayer }} </span>
+    </p>
+    <GameField
+      :board="board"
+      :current-board="currentBoard"
+      :win-board="winBoard"
+      :game-over="gameOver"
+      @playMove="playMove"
+    ></GameField>
+
+    <div class="">
+      <p v-if="winner">{{ winner }} wins!</p>
+      <p v-else-if="isTie">It's a tie!</p>
+      <button @click="reset">Reset Game</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-bottom: 0.5rem;
+}
+
+.current-player {
+  text-align: center;
+  color: #ccc;
+  font-size: 1.25rem;
+  margin-bottom: 1rem;
+}
+</style>
