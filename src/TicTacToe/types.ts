604bb465e82f73b47b9bc3b2c07d369d6212531f
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-12-14 19:02:30 +0200

    feat: added new types of game

--- src/TicTacToe/types.ts
+++ src/TicTacToe/types.ts
@@ -1,3 +1,18 @@
 export type Sign = 'X' | 'O' | ''
 export type Board = Sign[][][]
 export type SmallBoard = Sign[][]
+
+export enum GameType {
+  SinglePlayer,
+  MultiPlayer,
+}
+
+export enum SinglePlayerType {
+  Easy,
+  Medium,
+}
+
+export enum MultiPlayerType {
+  Local,
+  Online,
+}
