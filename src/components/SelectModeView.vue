820c6a494a859c6a1e197d5e175e405fcf551a1c
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-12-15 14:52:09 +0200

    feat: move App.vue functional to SelectModeView, update this component for router

---
+++ src/components/SelectModeView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { GameType } from '@/TicTacToe/types.ts'
+import { ref } from 'vue'
+import DynamicButton from '@/components/DynamicButton.vue'
+import DynamicLink from '@/components/DynamicLink.vue'
+
+const gameType = ref<GameType>()
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="game-type" v-if="gameType === undefined">
+      <DynamicButton @click="gameType = GameType.SinglePlayer"> Singleplayer</DynamicButton>
+      <DynamicButton @click="gameType = GameType.MultiPlayer"> Multiplayer</DynamicButton>
+    </div>
+
+    <div class="game-type" v-if="gameType === GameType.SinglePlayer">
+      <DynamicLink :to="'/bot/easy'"> AI Easy</DynamicLink>
+      <DynamicLink :to="'/bot/medium'"> AI Medium</DynamicLink>
+    </div>
+
+    <div class="game-type" v-if="gameType === GameType.MultiPlayer">
+      <DynamicLink :to="'/local'"> Local</DynamicLink>
+      <DynamicLink :to="'/multiplayer'"> Multiplayer</DynamicLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-bottom: 0.5rem;
+}
+
+.game-type {
+  display: flex;
+  column-gap: 1rem;
+  justify-content: space-between;
+}
+</style>
