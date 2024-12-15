ab290b1c0f2825506d08bb2cb32fea1ca92f3f92
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-12-15 15:17:06 +0200

    feat: create LocalGameView.vue

---
+++ src/components/game-views/LocalGameView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import GameView from '@/components/GameView.vue'
+import { GameType, MultiPlayerType } from '@/TicTacToe/types.ts'
+</script>
+
+<template>
+  <GameView
+    :game-type="GameType.SinglePlayer"
+    :multi-player-type="MultiPlayerType.Local"
+    :player="'X'"
+  ></GameView>
+</template>
+
+<style scoped></style>
